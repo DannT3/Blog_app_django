9c215bfae35f2b3ab63179e3b9273f10b3bd4f59
Daniil Tishin <danya.tishin.2002@mail.ru>
2025-05-17 02:21:07 +0300

    import get_object_or_404, change of post_detail view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
 
 def post_list(request):
     posts = Post.published.all()
@@ -11,10 +10,11 @@ def post_list(request):
     )
 
 def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("No post found")
+    post = get_object_or_404(
+        Post,
+        id=id,
+        status = Post.Status.PUBLISHED
+    )
     return render(
         request,
         'blog/post/detail.html',
