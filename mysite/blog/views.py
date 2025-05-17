b85b17580138a0b39c7d67e05858abdf94343369
Daniil Tishin <danya.tishin.2002@mail.ru>
2025-05-17 19:36:12 +0300

    created post_share view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView, DetailView
+from .forms import EmailPostform
+
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -21,5 +23,22 @@ def post_detail(request, year, month, day, post):
         request,
         'blog/post/detail.html',
         {'post': post}
+    )
+
+def post_share(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    if request.method == 'POST':
+        form = EmailPostform(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostform()
+    return render(
+        request,
+        'blog/post/share.html'
     )
 
