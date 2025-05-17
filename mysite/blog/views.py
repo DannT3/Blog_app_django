3b42ebaf76d559365edd51f4b089b5fb25b468b6
Daniil Tishin <danya.tishin.2002@mail.ru>
2025-05-17 19:17:09 +0300

    many changes

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,29 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView, DetailView
 
-
-def post_list(request):
-    post_list = Post.published.all()
-    paginator = Paginator(post_list, 3)  #Pagination with 3 posts per page
-    page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
-    return render(
-        request,
-        'blog/post/list.html',
-        {'posts': posts}
-    )
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
-        id=id,
-        status = Post.Status.PUBLISHED,
+        status=Post.Status.PUBLISHED,
         slug=post,
         publish__year=year,
         publish__month=month,
-        publish__day=day
-    )
+        publish__day=day)
     return render(
         request,
         'blog/post/detail.html',
